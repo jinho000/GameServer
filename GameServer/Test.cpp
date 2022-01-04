59f89fb8f5f84312668e3adabe8a47715b241a2e
jinho <jinho74350721@gmail.com>
2022-01-04 14:51:37 +0900

    IOCP 테스트 코드 추가

--- GameServer/Test.cpp
+++ GameServer/Test.cpp
@@ -1,7 +1,44 @@
 #include "pch.h"
 #include "Test.h"
 
+#include <conio.h>
+#include <GameServerBase/ServerIOCP.h>
+#include <GameServerBase/ServerIOCPWorker.h>
+
 void Test::TestServerBaseObject()
 {
 
+}
+
+void IOCPFunc(std::shared_ptr<ServerIOCPWorker> _iocpWorker)
+{
+	while (true)
+	{
+		IocpWaitReturnType returnType = _iocpWorker->Wait();
+
+		switch (returnType)
+		{
+		case IocpWaitReturnType::RETURN_TIMEOUT:
+			break;
+		case IocpWaitReturnType::RETURN_POST:
+			break;
+		case IocpWaitReturnType::RETURN_ERROR:
+			break;
+		default:
+			break;
+		}
+
+		std::cout << "test" << std::endl;
+	}
+}
+
+void Test::TestIOCP()
+{
+	ServerIOCP iocp(&IOCPFunc, 3, INFINITE);
+
+	while (true)
+	{
+		char c = _getch();
+		iocp.PostQueued(10, 10);
+	}
 }
--- GameServer/Test.h
+++ GameServer/Test.h
@@ -3,4 +3,5 @@
 namespace Test
 {
 	void TestServerBaseObject();
+	void TestIOCP();
 }
