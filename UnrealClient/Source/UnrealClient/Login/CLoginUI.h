0a6592ecffe2f0a731197f5c05b253871d377806
jinho <jinho74350721@gmail.com>
2022-02-07 06:41:27 +0900

    TItle level, Login level, Login UI 추가

---
+++ UnrealClient/Source/UnrealClient/Login/CLoginUI.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "CLoginUI.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALCLIENT_API UCLoginUI : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
