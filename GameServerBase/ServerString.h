e404a0ef112ef977d28c060f7964deaf21151554
jinho <jinho74350721@gmail.com>
2022-02-24 10:39:53 +0900

    서버에 필요한 스트링함수, 패킷제너레이터 프로젝트 생성

--- GameServerBase/ServerString.h
+++ GameServerBase/ServerString.h
@@ -34,5 +34,19 @@ public: // member Func
 	static bool ANSIToUTF8(const std::string& _ANSIStr, std::string& _UTF8StrBuffer);
 	static bool ANSIToUNICODE(const std::string& _ANSIStr, std::wstring& _UNICODEStrBuffer);
 	static bool UNICODEToUTF8(const std::wstring& _UNICODEStr, std::string& _UTF8StrBuffer);
+
+	static std::vector<std::string> split(const std::string& input, char delimiter);
+
+	static void Replace(std::string& _Text, const std::string& _Prev, const std::string& _Next, int _Count = 1);
+
+	static void Insert(std::string& _Text, int _Index, const std::string& _InsertText);
+
+	static void TrimRemove(std::string& _Text);
+
+	static void ToUpper(std::string& _Text);
+
+	static void Remove(std::string& _Text, const std::string& _DeleteText);
+
+	static void ClearText(std::string& _Text);
 };
 
